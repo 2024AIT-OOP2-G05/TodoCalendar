03040e7c86063fa9223da461d89d9266713731f7
KajitaTakaya <takaya0618.kjt@gmail.com>
2024-12-19 15:51:11 +0900

    Revert "detailPageV1"
    This reverts commit 1e1301ace674f439072c19ca4687dbc26fc1181c.

--- app.py
+++ app.py
@@ -17,11 +17,5 @@ def index():
 
     return render_template('calendar.html', year=year, month=month, month_days=month_days)
 
-@app.route('/detail/<int:year>/<int:month>/<int:day>')
-def detail(year, month, day):
-    # ここでdayに紐づく詳細情報を取得して表示する処理を書く
-    # とりあえず、デモとして日付を表示するだけ
-    return render_template('detail.html', year=year, month=month, day=day)
-
 if __name__ == '__main__':
     app.run(debug=True)
